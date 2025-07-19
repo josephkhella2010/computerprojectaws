64ee271c5ae12fb8e68f7fe7602c2279a6489101
Joseph Khella <josephkhella2010@gmail.com>
2025-07-19 11:14:29 +0200

    new header

--- frontend/computer/src/pages/Home/HomePage.tsx
+++ frontend/computer/src/pages/Home/HomePage.tsx
@@ -2,6 +2,11 @@ export default function HomePage() {
   return (
     <div>
       <h1>home page</h1>
+      <h1>home page</h1>
+      <h1>home page</h1>
+      <h1>home page</h1>
+      <h1>home page</h1>
+      <h1>home page</h1>
     </div>
   );
 }
