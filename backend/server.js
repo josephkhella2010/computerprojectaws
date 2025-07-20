13ea1ffe1c0e41712010932893e50bef80ccb20a
Joseph Khella <josephkhella2010@gmail.com>
2025-07-20 10:44:32 +0200

    delete product page

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,8 @@ app.use(cors());
 app.use(express.json());
 
 const getProducts = require("./ApiRouter/ProductsApi");
-const getComputerData = require("./ApiRouter/GetComputerData");
-const getSingleComputerData = require("./ApiRouter/GetSingleComputerData");
 
 app.use(getProducts);
-app.use(getComputerData);
-app.use(getSingleComputerData);
 
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
--- frontend/computer/src/pages/routes/RouterPage.tsx
+++ frontend/computer/src/pages/routes/RouterPage.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeSection from "../HomePage/HomeSection";
 import NavigationSection from "../Navigation/NavigationSection";
-import ProductSection from "../ProductPage/ProductSection";
-import SingleProductSection from "../singlePorductPage/SingleProductSection";
+//import ProductSection from "../ProductPage/ProductSection";
+//import SingleProductSection from "../singlePorductPage/SingleProductSection";
 import ContactUsPage from "../ContactUs/ContactUsPage";
 import AboutUsPage from "../aboutUs/AboutUsPage";
 import Footer from "../footer/Footer";
@@ -13,8 +13,8 @@ export default function RouterPage() {
       <NavigationSection />
       <Routes>
         <Route path="/" element={<HomeSection />} />
-        <Route path="/product" element={<ProductSection />} />
-        <Route path="/product/:id" element={<SingleProductSection />} />
+        {/*   <Route path="/product" element={<ProductSection />} />
+        <Route path="/product/:id" element={<SingleProductSection />} /> */}
         <Route path="/contactUs" element={<ContactUsPage />} />
         <Route path="/aboutUs" element={<AboutUsPage />} />
       </Routes>
